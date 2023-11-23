00d98573e3a9df1a0bfe6d734a0a3e64392095e9
Dinh Trung <trung.dinh@ntq-solution.com.vn>
2023-11-23 14:50:01 +0700

    update gulp to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
@@ -6,7 +7,14 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        filename: '[name].[chunkhash].bundle.js',
+        chunkFilename: '[name].bundle.js',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, './src/components')
+        }
     },
     module: {
         rules: [
@@ -18,19 +26,55 @@ module.exports = {
                 },
             },
             {
-                test: /\.scss$/i,
+                test: /\.(s(a|c)ss)$/,
                 use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader', 
-                    'sass-loader'
+                    //'style-loader', // include style tag to head html section, for development
+                    MiniCssExtractPlugin.loader, // for production
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                outputStyle: 'compressed'
+                            }
+                        }
+                    }
                 ],
             },
         ],
     },
+    devServer: {
+        historyApiFallback: true, // Fix can not get route error
+    },
     plugins: [
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'main.[hash].css',
+        }),
         new HtmlWebPackPlugin({
             template: './public/index.html',
         }),
     ],
+    optimization: {
+        minimize: true, // for development
+        minimizer: [ // for production
+            new CssMinimizerPlugin(),
+        ],
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    test: /node_modules\/(?!antd\/).*/,
+                    name: "vendors",
+                    chunks: "all",
+                },
+                antd: {
+                    test: /node_modules\/(antd\/).*/,
+                    name: "antd",
+                    chunks: "all",
+                },
+            },
+        },
+        runtimeChunk: {
+            name: "manifest",
+        },
+    }
 };
